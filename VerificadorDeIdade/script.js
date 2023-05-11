e1cabc5210dc3be0a4bf446e38af753221311fe2
Leandro Alves <leandroteclas1@gmail.com>
2023-05-11 20:32:51 -0300

    Desafios Curso em Vídeo

--- VerificadorDeIdade/script.js
+++ VerificadorDeIdade/script.js
@@ -21,22 +21,27 @@ function verificar() {
                 // Bebê
                 img.setAttribute('src', './assets/bebe-masc.png')
                 res.innerHTML = `Detectamos um bebê ${genero} com ${idade} anos.`
+                document.body.style.background = '#0092b1'
             } else if (idade < 21) {
                 // Jovem
                 img.setAttribute('src', './assets/jovem-masc.png')
                 res.innerHTML = `Detectamos um ${genero} jovem com ${idade} anos.`
+                document.body.style.background = '#014406'
             } else if (idade < 52) {
                 // Adulto
                 img.setAttribute('src', './assets/adulto-masc.png')
                 res.innerHTML = `Detectamos um ${genero} adulto com ${idade} anos.`
+                document.body.style.background = '#04047e'
             } else if (idade < 100) {
                 // Idoso
                 img.setAttribute('src', './assets/idoso-masc.png')
                 res.innerHTML = `Detectamos um ${genero} idoso com ${idade} anos.`
+                document.body.style.background = '#011937'
             } else {
                 // Faleceu
                 img.setAttribute('src', './assets/luto-1.png')
                 res.innerHTML = `✟ Provavelmente esta pessoa já faleceu ✟`
+                document.body.style.background = '#222932'
             }
 
         } else if (fsex[1].checked) {
@@ -46,18 +51,22 @@ function verificar() {
                 // Bebê
                 img.setAttribute('src', './assets/bebe-fem.png')
                 res.innerHTML = `Detectamos um bebê ${genero} com ${idade} anos.`
+                document.body.style.background = '#c49ef4'
             } else if (idade < 21) {
                 // Jovem
                 img.setAttribute('src', './assets/jovem-fem.png')
                 res.innerHTML = `Detectamos um ${genero} jovem com ${idade} anos.`
+                document.body.style.background = '#c58ef9'
             } else if (idade < 52) {
                 // Adulto
                 img.setAttribute('src', './assets/adulto-fem.png')
                 res.innerHTML = `Detectamos um ${genero} adulto com ${idade} anos.`
+                document.body.style.background = '#93019c'
             } else if (idade < 100) {
                 // Idoso
                 img.setAttribute('src', './assets/idosa-fem.png')
                 res.innerHTML = `Detectamos um ${genero} idoso com ${idade} anos.`
+                document.body.style.background = '#4f0601'
             } else {
                 // Faleceu
                 img.setAttribute('src', './assets/luto-1.png')
